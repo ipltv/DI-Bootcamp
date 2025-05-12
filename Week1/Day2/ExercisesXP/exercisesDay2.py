5216b4b6f8351ce675884b4f4c00fd420cbd3d36
platovich <platovich@yandex.ru>
2025-05-12 12:53:25 +0300

    Squences Exercise 8

--- Week1/Day2/ExercisesXP/exercisesDay2.py
+++ Week1/Day2/ExercisesXP/exercisesDay2.py
@@ -61,8 +61,23 @@ basket.clear()
 # Print the final state of the list.
 print("Final state of the basket:", basket)
 
+#Exercise 4
+print("--------------Exercise 4--------------")
 
+# Recap: What is a float? What’s the difference between a float and an integer?
+# Create a list containing the following sequence of mixed floats and integers:
+# 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5.
+# Avoid hard-coding each number manually.
+# Think: Can you generate this sequence using a loop or another method?
 
+numbers_list = []
+for i in range(3, 11):
+    x = i / 2
+    if x.is_integer():
+        numbers_list.append(int(x))
+    else:
+        numbers_list.append(x)
+print("My numbers list: ", numbers_list)
 
 #Exercise 5
 print("--------------Exercise 5--------------")
@@ -88,6 +103,93 @@ while True:
         break
     else:
         print("Your name is not Ilya.")
-        
 
+#Exercise 7
+print("--------------Exercise 7--------------")
+
+# Ask the user to input their favorite fruits (they can input several fruits, separated by spaces).
+# Store these fruits in a list.
+fruits_list = list(input("Enter your favorite fruits separated by spaces: ").split())
+print(fruits_list)
+
+# Ask the user to input the name of any fruit.
+user_fruit = input("Enter name of any frut:")
+
+# If the fruit is in their list of favorite fruits, print:
+# "You chose one of your favorite fruits! Enjoy!"
+# If not, print:
+# "You chose a new fruit. I hope you enjoy it!" 
+if user_fruit in fruits_list:
+    print("You chose one of your favorite fruits! Enjoy!")
+else:
+    print("You chose a new fruit. I hope you enjoy it!" )
+
+#Exercise 8
+print("--------------Exercise 8--------------")
+
+# Write a loop that asks the user to enter pizza toppings one by one.
+# Stop the loop when the user types 'quit'.
+# For each topping entered, print:
+# "Adding [topping] to your pizza."
+topping = ""
+toppings_list = []
+base_price = 10
+add_topping_price = 2.5
+
+while True:
+    topping = input("Enter pizza topping (for the stop enter quit): ")
+    if topping != "quit":
+        toppings_list.append(topping)
+        print(f"Adding {topping} to your pizza.")
+    else:
+        break
+    
+# After exiting the loop, print all the toppings and the total cost of the pizza.
+# The base price is $10, and each topping adds $2.50.
+if len(toppings_list) > 0:
+    print("You chose: ", toppings_list)
+    print(f"The price is: {base_price + (len(toppings_list)-1)*add_topping_price}")
+else:
+    print("You didn't choose any toppings :(")
+
+#Exercise 9
+print("--------------Exercise 9--------------")
+
+# Ask for the age of each person in a family who wants to buy a movie ticket.
+# Calculate the total cost based on the following rules:
+# Free for people under 3.
+# $10 for people aged 3 to 12.
+# $15 for anyone over 12.
+# Print the total ticket cost.
+
+babysTicketCost = 0
+babysMaxAge = 3
+
+kidsTicketCost = 10
+kidsMaxAge = 12
+
+fullTicketCost = 15
+
+price = 0
+input_age = 0
+while input_age != "quit":
+    input_age = input("Enter your age (enter quit for the stop): ")
+    try:
+        input_age = int(input_age)
+        if input_age < 0:
+            raise ValueError("Age cannot be negative.")
+    except ValueError:
+        print("You put something strange. Try it one more time.")
+    else:
+        if input_age < babysMaxAge:
+            price += babysTicketCost
+            print(f"This ticket costs: {babysTicketCost}. Go to the next.")
+        elif input_age <= kidsMaxAge:
+            price += kidsTicketCost
+            print(f"This ticket costs: {kidsTicketCost}. Go to the next.")
+        else:
+            price += fullTicketCost
+            print(f"This ticket costs: {fullTicketCost}. Go to the next.")
+
+print("Your total price is $", price)
 #The End

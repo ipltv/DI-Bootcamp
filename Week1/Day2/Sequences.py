5432177110da0805b1c520bbd5252c603dc148e0
platovich <platovich@yandex.ru>
2025-05-12 09:50:41 +0300

    Squences Exercise 2

--- Week1/Day2/Sequences.py
+++ Week1/Day2/Sequences.py
@@ -11,3 +11,21 @@ print(a) # should print 10
 print(b) # should print 20
 print(c) # should print 30
 print(d) # should print 40
+
+#Exercise 2
+print("--------------Exercise 2--------------")
+
+while True:
+    try:
+        a = int(input("Enter a difgit: "))
+    except ValueError:
+        print("Please enter valid integers.")
+    else:
+        for i in range(1, 11):
+            print(f"{a} x {i} = {a * i}")
+        break
+
+#Exercise 3
+print("--------------Exercise 3--------------")
+
+    

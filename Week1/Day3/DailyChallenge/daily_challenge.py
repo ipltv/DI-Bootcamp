b604268dcd35a73ca14f43afdcd5a9f71bf8a3b8
platovich <platovich@yandex.ru>
2025-05-13 13:10:03 +0300

    Daily Exercises XP done

--- Week1/Day3/DailyChallenge/daily_challenge.py
+++ Week1/Day3/DailyChallenge/daily_challenge.py
@@ -63,3 +63,72 @@ print(f"Total cost of movie tickets: ${total_cost}")  # Output: Total cost of mo
 #Exercise 3: Zara
 print("--------------Exercise 3--------------")
 
+#Create and manipulate a dictionary that contains information about the Zara brand.
+# Create a dictionary called brand with the provided data.
+# Modify and access the dictionary as follows:
+# Change the value of number_stores to 2.
+# Print a sentence describing Zara’s clients using the type_of_clothes key.
+# Add a new key country_creation with the value Spain.
+# Check if international_competitors exists and, if so, add “Desigual” to the list.
+# Delete the creation_date key.
+# Print the last item in international_competitors.
+# Print the major colors in the US.
+# Print the number of keys in the dictionary.
+# Print all keys of the dictionary.
+# Bonus. Create another dictionary called more_on_zara with creation_date and number_stores. Merge this dictionary with the original brand dictionary and print the result.
+
+brand = {
+    "name": "Zara",
+    "creation_date": 1975,
+    "creator_name": "Amancio Ortega Gaona",
+    "type_of_clothes": ["men", 'women', 'children', 'home'],
+    "international_competitors": ['Gap', 'H&M', 'Benetton'],
+    "number_stores": 7000,
+    "major_color": {
+        'France': 'blue', 
+        'Spain': 'red', 
+        'US': ['pink', 'green']
+    }
+}
+
+brand["number_stores"] = 2
+print(f"Zara's clients are using {brand['type_of_clothes']} type of clothes.")
+brand["country_creation"] = "Spain"
+print("Added creation country to the dictionary: ", brand["country_creation"])
+if "international_competitors" in brand:
+    brand["international_competitors"].append("Desigual")
+    print("Added Desigual to international competitors: ", brand["international_competitors"])
+brand.pop("creation_date", None)
+print("Deleted creation date from the dictionary.")
+print("Last item in international competitors: ", brand["international_competitors"][-1])
+print("Major colors in the US: ", brand["major_color"]["US"])
+print("Number of keys in the dictionary: ", len(brand))
+print("All keys in the dictionary: ", brand.keys())
+
+#Bonus: Merge the two dictionaries
+more_on_zara = {
+    "creation_date": 1976,
+    "number_stores": 7001
+}
+print("More on Zara: ", more_on_zara)
+
+brand.update(more_on_zara)
+print("Merged dictionary: ", brand)
+
+#Exercise 4: Disney Characters
+print("--------------Exercise 4--------------")
+#You are given a list of Disney characters. Create three dictionaries based on different patterns as shown below:
+
+users = ["Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+
+#Create a dictionary that maps characters to their indices:
+user_dict = {user: index for index, user in enumerate(users)}
+print("Dictionary mapping characters to their indices: ", user_dict)
+
+#Create a dictionary that maps indices to characters:
+indicas_dict = {index: user for index, user in enumerate(users)}
+print("Dictionary mapping indices to characters: ", indicas_dict)
+
+#Create a dictionary where characters are sorted alphabetically and mapped to their indices:
+alphabetical_dict = {user: index for index, user in enumerate(sorted(users))}
+print("Dictionary mapping characters to their indices in alphabetical order: ", alphabetical_dict)

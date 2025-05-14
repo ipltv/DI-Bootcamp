aa0c207cf1d469de7f6fa6fe59f6e82a35cd288e
platovich <platovich@yandex.ru>
2025-05-14 12:04:32 +0300

    Day 3 Gold Exercises

--- Week1/Day3/ExercisesXP/exercisesDay3Gold.py
+++ Week1/Day3/ExercisesXP/exercisesDay3Gold.py
@@ -1,2 +1,74 @@
 #Exercise 1: Birthday Look-up
 print("--------------Exercise 1--------------")
+
+birthdays = {
+    "Alice": "1995/04/01",
+    "Bob": "1996/04/01",
+    "Charlie": "2000/07/05",
+    "David": "2005/07/04",
+    "Eve": "2001/07/04",
+}
+
+name = input("Enter a name: ")
+if name in birthdays:
+    print(f"{name}'s birthday is {birthdays[name]}")
+else:
+    print(f"{name} not ound in the list.")
+
+#Exercise 2: Birthdays Advanced
+print("--------------Exercise 2--------------")
+
+print("We have these names in the list:")
+for name in birthdays.keys():
+    print(name)
+print("Please enter a name from the list:")
+name = input("Enter a name: ")
+if name in birthdays:
+    print(f"{name}'s birthday is {birthdays[name]}")
+else:
+    print(f"Sorry, we don’t have the birthday information for {name}.")
+    
+#Exercise 3: Add Your Own Birthday
+print("--------------Exercise 3--------------")
+
+userName = input("Please enter your name: ")
+userBirthday = input("Please enter your birthday (YYYY/MM/DD): ")
+
+birthdays[userName] = userBirthday
+
+print("We have these names in the list:")
+for name in birthdays.keys():
+    print(name)
+print("Please enter a name from the list:")
+name = input("Enter a name: ")
+if name in birthdays:
+    print(f"{name}'s birthday is {birthdays[name]}")
+else:
+    print(f"Sorry, we don’t have the birthday information for {name}.")
+
+#Exercise 4: Fruit Shop
+print("--------------Exercise 4--------------")
+
+items = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+for item in items:
+    print(f"{item}: ${items[item]}")
+
+print("---Stock information---") 
+items = {
+    "banana": {"price": 4 , "stock":10},
+    "apple": {"price": 2, "stock":5},
+    "orange": {"price": 1.5 , "stock":24},
+    "pear": {"price": 3 , "stock":1}
+}
+
+sum = 0
+for item in items:
+    print (f"{item}: ${items[item]['price']} Stock: {items[item]['stock']}")
+    sum += items[item]['price'] * items[item]['stock']
+print(f"Total: ${sum}")
--- Week1/Day4/function.py
+++ Week1/Day4/function.py
@@ -1,5 +1,31 @@
-def helloInRussian():
-    print("Привет!")
-    
-helloInRussian()
-helloInRussian()
+def greetings(language, userName):
+    '''This function prints "Hello" in some languages.'''
+    result = ""
+    if language == "EN":
+        print(f"Hello, {userName}!")
+    elif language == "SP":
+        print(f"¡Hola, {userName}!")
+    elif language == "FR":
+        print(f"Bonjour, {userName}!")
+    elif language == "HE":
+        print(f"שלום, {userName}!")
+    elif language == "RU":
+        print(f"Привет, {userName}!")
+    else:
+        print("Language not supported!")
+greetings("HE", "David")
+
+def country_info(country="Naboo"):
+    if country == "Naboo":
+        print(f"The capital of {country} is Theed.")
+    elif country == "Italy":
+        print(f"The capital of {country} is Rome.")
+    elif country == "France":
+        print(f"The capital of {country} is Paris.")
+    elif country == "Israel":
+        print(f"The capital of {country} is Jerusalem.")
+    elif country == "Russia":
+        print(f"The capital of {country} is Moscow.")
+        
+country_info("Italy")
+country_info()

3fd366c18314a10c2cf1b1dee6e61f30d47b4d3d
platovich <platovich@yandex.ru>
2025-05-14 13:45:49 +0300

    Day 3 Time Challenge 1

---
+++ Week1/Day3/ExercisesXP/timeChallenge1.py
@@ -0,0 +1,13 @@
+REverseinp= input("Enter text for reverse:")
+
+
+#write down your logic here 
+
+wordList = REverseinp.split()
+print(wordList)
+reversed = ' '.join([word for word in reversed(wordList)])
+# 
+
+print(reversed) 
+
+#This challenge took 5 min

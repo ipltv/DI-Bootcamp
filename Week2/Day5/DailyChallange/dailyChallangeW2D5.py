acfdcb343c0604a40eec6be67858d05c977c83a8
platovich <platovich@yandex.ru>
2025-05-23 23:49:45 +0300

    W2D4 Daily Challange Start

---
+++ Week2/Day5/DailyChallange/dailyChallangeW2D5.py
@@ -0,0 +1,22 @@
+# Exercise 1: Quizz
+# Answer the following questions:
+
+# Q: What is a class?
+# A: Class is speatial structure that represent a bluebrint for creating instances. 
+# Class usually contains basic value data types as fields, properties and methods.
+
+# Q: What is an instance?
+# A: Instance 
+
+# Q: What is encapsulation?
+# A:
+# Q: What is abstraction?
+# A:
+# Q: What is inheritance?
+# A:
+# Q: What is multiple inheritance?
+# A:
+# Q: What is polymorphism?
+# A:
+# Q: What is method resolution order or MRO?
+# A:

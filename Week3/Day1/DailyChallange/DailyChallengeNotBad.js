9fa120e08aecd1da2ab1448848421bcbbe682438
platovich <platovich@yandex.ru>
2025-05-25 14:23:37 +0300

    W3D1 DailyChallenge Not Bad

---
+++ Week3/Day1/DailyChallange/DailyChallengeNotBad.js
@@ -0,0 +1,15 @@
+let sentence = "The movie is not that bad, I like it";
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+let newSentence = ""
+if (wordBad > wordNot && wordNot != -1){
+    newSentence = sentence.slice(0,wordNot) + "good" + sentence.slice(wordBad + "bad".length)
+    console.log(newSentence)
+}
+else{
+    newSentence = sentence
+}
+
+console.log("Your string is : ", sentence)
+console.log("--> the result is : ", newSentence)

88131871566c9095ec1f531b5b3cc99ce97f23d4
platovich <platovich@yandex.ru>
2025-05-27 16:11:13 +0300

    W3D3 DailyChallange template

---
+++ Week3/Day3/DailyChallange/script.js
@@ -0,0 +1,5 @@
+// One day, [person] decided to [verb] at the [place] with a [adjective] [noun].
+// Everyone knew that [person] kept a [noun] in the [place], because it was too [adjective] to [verb] at home.
+// When the [place] suddenly disappeared, only a [adjective] [noun] and [person] could [verb] it.
+// Yesterday, [person] bought a [adjective] [noun] and decided to [verb] with it right at the [place].
+// Nobody expected the [noun] to be so [adjective] when [person] started to [verb] in the [place].

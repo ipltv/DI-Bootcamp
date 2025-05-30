34f3707c6c1bd2e1042ab763dc0aa70098ceb1fd
platovich <platovich@yandex.ru>
2025-05-30 18:41:29 +0300

    W3D5 DailyChallenge base logic done

--- Week3/Day5/DailyChallenge/script.js
+++ Week3/Day5/DailyChallenge/script.js
@@ -1,53 +1,80 @@
-const tasks = [];
+const tasks = {};
+let tasksCounter = 0;
 let tasksDiv = document.querySelector("#listTasks");
+const input = document.getElementById("newTaskTextField");
 let form = document.forms.item(0);
 let ul = document.querySelector("ul");
 
 console.log(tasksDiv);
 console.log(ul);
 
-function getRedCrosElement(li) {
-    let i = document.createElement("i");
-    i.classList.add("fa");
-    i.classList.add("fa-close");
-    i.style.color = "red";
-    i.addEventListener("click", () => {
+function doneTask(event) {
+    const isChecked = event.target.checked;
+    const taskId = event.target.dataset.taskId;
+    tasks[taskId].done = isChecked;
+    const label = document.querySelector(`label[for='${taskId}']`);
+    if (label) {
+        label.style.textDecoration = isChecked ? "line-through" : "";
+        label.style.color = isChecked ? "grey" : "black";
+    }
+}
+
+    function deleteTask(li) {
+        const checkbox = li.querySelector("input[type='checkbox']");
+        const taskId = checkbox.dataset.taskId;
+        delete tasks[taskId];
         li.remove();
-    })
-    return i;
-}
+    }
 
-function getLabel(inputValue, taskId){
-    let label = document.createElement("label");
-    label.setAttribute("for",taskId);
-    label.append(inputValue); 
-    return label;
-}
+    function getRedCrossElement(li) {
+        let i = document.createElement("i");
+        i.classList.add("fa");
+        i.classList.add("fa-close");
+        i.style.color = "red";
+        i.style.cursor = "pointer";
+        i.addEventListener("click", () => {
+            deleteTask(li);
+        })
+        return i;
+    }
 
-function getCheckBox(inputValue, taskId){
-    let checkBox = document.createElement("input");
-    checkBox.setAttribute("type", "checkbox");
-    checkBox.setAttribute("id", taskId);
-    return checkBox;
-}
+    function getLabel(inputValue, taskId) {
+        let label = document.createElement("label");
+        label.setAttribute("for", taskId);
+        label.append(inputValue);
+        return label;
+    }
 
-function getLiElement(inputValue, taskId) {
-    let li = document.createElement("li");
-    li.appendChild(getRedCrosElement(li));
-    li.appendChild(getCheckBox(inputValue, taskId));
-    li.appendChild(getLabel(inputValue,taskId));
-    return li;
-}
+    function getCheckBox(taskId) {
+        let checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("data-task-id", taskId);
+        checkBox.setAttribute("id", taskId);
+        return checkBox;
+    }
 
-function addTask() {
-    let inputValue = document.getElementById("newTaskTextField").value.trim();
-    if (!inputValue) return;
-    tasks.push(inputValue);
-    let taskId = `userTask#${tasks.length}`
-    ul.appendChild(getLiElement(inputValue, taskId));
-}
+    function getLiElement(taskID) {
+        let li = document.createElement("li");
+        let redCross = getRedCrossElement(li);
+        let checkBox = getCheckBox(taskID);
+        let label = getLabel(tasks[taskID].textValue, taskID);
+        li.appendChild(redCross);
+        li.appendChild(checkBox);
+        li.appendChild(label);
+        checkBox.addEventListener("change", (e) => doneTask(e))
+        return li;
+    }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addTask();
-});
+    function addTask() {
+        let inputValue = document.getElementById("newTaskTextField").value.trim();
+        if (!inputValue) return;
+        tasks[tasksCounter] = { textValue: inputValue, done: false }
+        ul.appendChild(getLiElement(tasksCounter));
+        tasksCounter += 1;
+        input.value = "";
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        addTask();
+    });

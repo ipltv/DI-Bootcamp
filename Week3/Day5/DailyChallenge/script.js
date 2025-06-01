7375af11fb7df1de35f5f09b1d378781e0b22142
platovich <platovich@yandex.ru>
2025-06-01 11:47:23 +0300

    Reverted DailyChallenge to previous state

--- Week3/Day5/DailyChallenge/script.js
+++ Week3/Day5/DailyChallenge/script.js
@@ -1,78 +1,80 @@
-colors = [
-    "red",
-    "orangered",
-    "orange",
-    "yellow",
-    "yellowgreen",
-    "lightgreen",
-    "green",
-    "turquoise",
-    "cyan",
-    "lightskyblue",
-    "dodgerblue",
-    "blue",
-    "darkblue",
-    "indigo",
-    "darkmagenta",
-    "violet",
-    "lightpink",
-    "lightgray",
-    "gray",
-    "black",
-    "white"
-]
+const tasks = {};
+let tasksCounter = 0;
+let tasksDiv = document.querySelector("#listTasks");
+const input = document.getElementById("newTaskTextField");
+let form = document.forms.item(0);
+let ul = document.querySelector("ul");
 
-const sideBarDiv = document.getElementById("sidebar");
-const fieldDiv = document.getElementById("field");
-const clearButton = document.getElementsByTagName("button")[0];
-let chosenColor = "white";
-let isMouseDown = false;
+console.log(tasksDiv);
+console.log(ul);
 
-function paintCell(e) {
-    e.target.style.backgroundColor = chosenColor;
-}
-
-
-function generatePallet() {
-    for (let i = 0; i < colors.length; i++) {
-        let palleteElement = document.createElement("div");
-        palleteElement.style.backgroundColor = colors[i];
-        sideBarDiv.appendChild(palleteElement);
-        palleteElement.addEventListener("click", (e) => {
-            chosenColor = e.target.style.backgroundColor;
-            for (let child of sideBarDiv.children) {
-                child.style.border = "1px solid black";
-            }
-            e.target.style.border = "3px solid deeppink";
-        })
+function doneTask(event) {
+    const isChecked = event.target.checked;
+    const taskId = event.target.dataset.taskId;
+    tasks[taskId].done = isChecked;
+    const label = document.querySelector(`label[for='${taskId}']`);
+    if (label) {
+        label.style.textDecoration = isChecked ? "line-through" : "";
+        label.style.color = isChecked ? "#FF0101" : "black";
     }
 }
 
-function generateField() {
-    for (let i = 0; i < 1440; i++) {
-        let gridElement = document.createElement("div");
-        fieldDiv.appendChild(gridElement);
-
-        gridElement.addEventListener("mousedown", (e) => {
-            isMouseDown = true;
-            paintCell(e);
-        })
-
-        gridElement.addEventListener("mouseover", (e) => {
-            if (isMouseDown) paintCell(e);
-        })
+    function deleteTask(li) {
+        const checkbox = li.querySelector("input[type='checkbox']");
+        const taskId = checkbox.dataset.taskId;
+        delete tasks[taskId];
+        li.remove();
     }
-}
 
-
-document.addEventListener("mousedown", () => isMouseDown = true);
-document.addEventListener("mouseup", () => isMouseDown = false);
-
-clearButton.addEventListener("click", () => {
-    for (let cell of fieldDiv.children) {
-        cell.style.backgroundColor = "white";
+    function getRedCrossElement(li) {
+        let i = document.createElement("i");
+        i.classList.add("fa");
+        i.classList.add("fa-close");
+        i.style.color = "red";
+        i.style.cursor = "pointer";
+        i.addEventListener("click", () => {
+            deleteTask(li);
+        })
+        return i;
     }
-})
 
-generatePallet();
-generateField();
+    function getLabel(inputValue, taskId) {
+        let label = document.createElement("label");
+        label.setAttribute("for", taskId);
+        label.append(inputValue);
+        return label;
+    }
+
+    function getCheckBox(taskId) {
+        let checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("data-task-id", taskId);
+        checkBox.setAttribute("id", taskId);
+        return checkBox;
+    }
+
+    function getLiElement(taskID) {
+        let li = document.createElement("li");
+        let redCross = getRedCrossElement(li);
+        let checkBox = getCheckBox(taskID);
+        let label = getLabel(tasks[taskID].textValue, taskID);
+        li.appendChild(redCross);
+        li.appendChild(checkBox);
+        li.appendChild(label);
+        checkBox.addEventListener("change", (e) => doneTask(e))
+        return li;
+    }
+
+    function addTask() {
+        let inputValue = document.getElementById("newTaskTextField").value.trim();
+        if (!inputValue) return;
+        tasks[tasksCounter] = { textValue: inputValue, done: false }
+        ul.appendChild(getLiElement(tasksCounter));
+        tasksCounter += 1;
+        input.value = "";
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        addTask();
+    });

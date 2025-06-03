acaa40f04181a392cb1fe242a7f9f3196db15297
platovich <platovich@yandex.ru>
2025-06-03 14:12:05 +0300

    W4D1 DailyChallenge Car Inventory Done

---
+++ Week4/Day1/DailyChallengeCarInventory/script.js
@@ -0,0 +1,21 @@
+const inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+function getCarHonda(carInventory){
+  let firstHonda  = carInventory.find((item) => item.car_make == "Honda");
+  return `This is a ${firstHonda.car_make} ${firstHonda.car_model} from ${firstHonda.car_year}`;
+}
+
+console.log(getCarHonda(inventory));
+
+function sortCarInventoryByYear(carInventory) {
+  return carInventory.sort((a,b) => a.car_year - b.car_year);
+}
+
+console.log(inventory);
+console.log(sortCarInventoryByYear(inventory));

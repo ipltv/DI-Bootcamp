3e2c1996d61ee583df3abe40d4766df77f589178
platovich <platovich@yandex.ru>
2025-06-09 13:09:50 +0300

    W3D5 ExercisesXP Done

--- Week4/Day5/ExercisesXP/script.js
+++ Week4/Day5/ExercisesXP/script.js
@@ -1,68 +1,87 @@
-// // Exercise 1 : Giphy API
-// let API_KEY;
+// Exercise 1 : Giphy API
+let API_KEY;
 
-// function getURL(query, startingPosition, rating, limit, API_KEY)
-// {
-//     return `https://api.giphy.com/v1/gifs/search?q=${query}&rating=${rating}&offset=${startingPosition}&limit=${limit}&api_key=${API_KEY}`
-// }
+function getURL(query, startingPosition, rating, limit, API_KEY)
+{
+    return `https://api.giphy.com/v1/gifs/search?q=${query}&rating=${rating}&offset=${startingPosition}&limit=${limit}&api_key=${API_KEY}`
+}
 
-// async function loadKey() {
-//   try {
-//     const res = await fetch('./giphyAPI.txt');
-//     return await res.text();
-//   } catch (err) {
-//     console.error("Error loading API key:", err);
-//     throw err;
-//   }
-// }
+async function loadKey() {
+  try {
+    const res = await fetch('./giphyAPI.txt');
+    return await res.text();
+  } catch (err) {
+    console.error("Error loading API key:", err);
+    throw err;
+  }
+}
 
-// async function retrieveData(query, startingPosition, rating, limit=50) {
-//   try {
-//     API_KEY = await loadKey();
-//     console.log("Loaded API_KEY:", API_KEY);
+async function retrieveData(query, startingPosition, rating, limit=50) {
+  try {
+    API_KEY = await loadKey();
+ 
+    const endpoint = getURL(query, startingPosition, rating, limit, API_KEY);
+    const res = await fetch(endpoint);
 
-//     const endpoint = getURL(query, startingPosition, rating, limit, API_KEY);
-//     const res = await fetch(endpoint);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
 
-//     if (!res.ok) {
-//       throw new Error(`HTTP error! status: ${res.status}`);
-//     }
+    const data = await res.json();
+    console.log("Fetched Giphy Data:", data);
+    return data;
+  } catch (err) {
+    console.error("Error retrieving data:", err);
+  }
+}
 
-//     const data = await res.json();
-//     console.log("Fetched Giphy Data:", data);
-//     return data;
-//   } catch (err) {
-//     console.error("Error retrieving data:", err);
-//   }
-// }
+let hilariousResultObj;
+retrieveData("hilarious", 0, "g").then(data => {
+  if (data) {
+    hilariousResultObj = data;
+  }
+});
 
-// let hilariousResultObj;
-// retrieveData("hilarious", 0, "q").then(data => {
-//   if (data) {
-//     hilariousResultObj = data;
-//   }
-// });
+// Exercise 2 : Gaiphy API
 
-// Exercise 2 : Giphy API
-
-// let sunResultObj;
-// retrieveData("sun", 0, "r",10).then(data => {
-//   if (data) {
-//     sunResultObj = data;
-//   }
-// });
+let sunResultObj;
+retrieveData("sun", 2, "r",10).then(data => {
+  if (data) {
+    sunResultObj = data;
+  }
+});
 
 // Exercise 3 : Async function
 async function swapi() {
-    let response = await fetch("https://www.swapi.tech/api/starships/9/");
-    // .then(response => response.json())
-    // .then(objectStarWars => console.log(objectStarWars.result));
-    if (!response.ok) {
-        throw new Error("Request is not succsessful.");
+    try{
+        const response = await fetch("https://www.swapi.tech/api/starships/9/");
+        if (!response.ok) {
+            throw new Error("Request is not succsessful.");
+        }
+        let data = await response.json()
+        console.log(data.result);
+    } catch(error){
+        console.error(error);
     }
-    let data = await response.json()
-    console.log(data.result);
 }
-
 swapi();
 
+// Exercise 4: Analyze
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    let result = await resolveAfter2Seconds();
+    console.log(result);
+}
+
+asyncCall();
+
+//Expected outcome: Initialy asyncCall print "calling" to console, then it will output 'resolved' into conlose after 2 seconds. 

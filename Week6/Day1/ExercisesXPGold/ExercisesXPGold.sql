d1f059173028a9a7497c6657938629ea6d5ee0a1
platovich <platovich@yandex.ru>
2025-06-23 01:52:12 +0300

    W6D1 ExercisesGold Done

---
+++ Week6/Day1/ExercisesXPGold/ExercisesXPGold.sql
@@ -0,0 +1,12 @@
+-- Database: bootcamp
+-- Exrecises XP Gold
+
+SELECT * FROM students;
+-- Fetch the first four students. You have to order the four students alphabetically by last_name.
+SELECT first_name, last_name, birth_date FROM students ORDER BY last_name ASC LIMIT 4;
+
+-- Fetch the details of the youngest student.
+SELECT first_name, last_name, birth_date FROM students ORDER BY birth_date DESC LIMIT 1;
+
+-- Fetch three students skipping the first two students.
+SELECT first_name, last_name, birth_date FROM students OFFSET 2 LIMIT 3;

1bb224a9814586e9fee138c6bb1ab3200f1f15c5
platovich <platovich@yandex.ru>
2025-07-17 14:09:56 +0300

    Daily Challange Done

---
+++ Week7/Day5/DailyChallenge/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [languages, setLanguages] = useState([
+    { name: "Php", votes: 0 },
+    { name: "Python", votes: 0 },
+    { name: "JavaScript", votes: 0 },
+    { name: "Java", votes: 0 }
+  ])
+
+  const handleVote = (index) => {
+    const newLangs = [...languages];
+    newLangs[index].votes += 1;
+    setLanguages(newLangs);
+  }
+
+  return (
+    <>
+      <h1>Vote Your Language!</h1>
+      {languages?.map((item, index) => {
+        return (
+          <div key={index} className='lanBlock'>
+            <span>{item.votes}</span>
+            <span>{item.name}</span>
+            <button onClick={() => handleVote(index)}>Click Here</button>
+          </div>
+        );
+      })
+      }
+    </>
+  )
+}
+
+export default App

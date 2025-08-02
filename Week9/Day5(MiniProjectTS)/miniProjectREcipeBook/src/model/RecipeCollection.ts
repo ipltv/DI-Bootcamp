765157b2f51ebe43b3f2fb994bc74748538f260f
platovich <platovich@yandex.ru>
2025-08-02 17:06:05 +0300

    W9D5 miniProject RecipeCollection Class was implemented

--- Week9/Day5(MiniProjectTS)/miniProjectREcipeBook/src/model/RecipeCollection.ts
+++ Week9/Day5(MiniProjectTS)/miniProjectREcipeBook/src/model/RecipeCollection.ts
@@ -4,19 +4,26 @@ export class RecipeCollection {
     private recipes: Recipe[] = [];
 
     addRecipe(recipe: Recipe): void {
-        throw new Error("Method is not implemented yet.");
+        this.recipes.push(recipe);
     }
     removeRecipe(id: string): void {
-        throw new Error("Method is not implemented yet.");
+        this.recipes = this.recipes.filter(item => item.id !== id);
     }
     toggleFavoriteRecipe(id: string): void {
-        throw new Error("Method is not implemented yet.");
+        const recipe = this.recipes.find(item => item.id === id);
+        if (recipe) {
+            recipe.isFavorite = !recipe.isFavorite;
+        }
     }
     saveToLocalStorage(): void {
-        throw new Error("Method is not implemented yet.");
+        localStorage.setItem("RecipeCollection", JSON.stringify(this.recipes));
     }
     loadFromLocalStorage(): void {
-        throw new Error("Method is not implemented yet.");
+        const dataFromStorage = localStorage.getItem("RecipeCollection")
+        const parsed = dataFromStorage ? JSON.parse(dataFromStorage) as Recipe[] : null;
+        if (Array.isArray(parsed)) {
+            this.recipes = parsed;
+        }
     }
     getAllRecipes(): Recipe[] {
         return [...this.recipes];
